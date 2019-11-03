2145a3af853ce82f321db265d95456d9297275b1
Krishna <kaddala@salesforce.com>
2019-11-02 17:37:44 -0700

    adding HTML tags for holding giphy's and minor css updates with JS pseudo code

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,11 @@
+//Initial Pseudo code for the assignment:
+//create a list of superhero array
+//assign the superhero to a respective Btn on the HTML page
+// Upon Click of each superhero button, display the related info by making an API call using AJAX method
+// Display list of information of each superhero with Rating, title and any other related tags to display on HTML
+//In the search bar, when the user enters a superhero name of their choice
+// we should dynamically create a button on the searched name
+// when the user clicks on the newly added button, it should once again display all related info...
+
+
+var superheroList = ["Superman", "Iron Man", "Batman", "Captain America", "Flash", "Thor", "Green Lantern", "Black Panther", "One Punch Man"];
